948acca1f7ea1858200ff1a6206d0df9db141fd5
raphael.amar <raphael.amar@live.fr>
2013-07-21 15:32:35 +0200

    Amélioration partie UX et mise en place du choix par couleur. DashBoard Powa !

--- src/Datacity/PrivateBundle/Resources/public/js/api.js
+++ src/Datacity/PrivateBundle/Resources/public/js/api.js
@@ -1,11 +1,15 @@
 var erpModel = {
 	"id" 			: null,
 	"nom"			: null,
-	"titre"			: null,
+	"categorie"		: null,
 	"description" 	: null,
 	"longitude" 	: null,
-	"latitude"		: null
+	"latitude"		: null,
+	"adresse"		: null
 };
+
+var colors = ["purple", "green", "orange", "pink", "brown", "red", "blue"];
+var currentColor;
 
 var callAjax = function(jsonobject) {
 	$.ajax({
@@ -27,10 +31,18 @@ $('#public-service').click(function() {
 	$('.models').html(generateModel(erpModel));
 });
 
+$('body').on('click', '.modelfield', function() {
+	$('.boxColored').css('background-color', $(this).css('background-color'));
+	currentColor = $(this).css('background-color');
+});
+
 var generateModel = function(model) {
 	var result = '';
+	console.log("coucou");
+	var i = 0;
 	for (field in model) {
-		result += '<br/><button class="btn btn-default" type="button">' + field + '</button><br/>';
+		result += '<br/><button class="btn modelfield btn-default btn-' + colors[i] + '" type="button">' + field + '</button><br/>';
+		i++;
 	}
 	return result;
 };
--- src/Datacity/PrivateBundle/Resources/public/js/dynamicDataTables.js
+++ src/Datacity/PrivateBundle/Resources/public/js/dynamicDataTables.js
@@ -34,28 +34,28 @@ function generateHeaders(headers) {
 }
 
 function generateDataTables(headers, rows, tableName) {
-    $('#datatable' + example).append("<div class='box-header blue-background'><div class='title'>" + tableName + "</div></div><br>");
-	$('#datatable' + example).append( '<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="tableopt' + example + '"></table>' );
+   	example--
+   	$('#tab' + example).removeClass('active');
+   	$('.current' + example).removeClass('active');
+   	example++;
+   	$('#tab' + example).addClass('active');
+   	$('.current' + example).addClass('active');
+    $('#tab' + example).append("<div class='box-header blue-background'><div class='title'>" + tableName + "</div></div><br>");
+	$('#tab' + example).append( '<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="tableopt' + example + '"></table>' );
     $('#tableopt' + example).dataTable( {
-    	"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
 		"sScrollY": "200px",
 		"sScrollX": "900px",
+		"bSort": false,
 		"oLanguage": {
 			"sLengthMenu": "_MENU_ records per page"
 		},
 		"iDisplayLength": 30,
-    	"aLengthMenu": [[30, 50, 100, 500, -1], [30, 50, 100, 500, "All"]],
+    	"aLengthMenu": [[30, 50, 100, 500, -1], [30, 50, 100, 500, "Tous"]],
         "aaData": generateRows(rows, headers.length),
         "aoColumns": generateHeaders(headers)
     } );
     example++;
 }
-
-$('#public-service').click(function() {
-	$('.models').html(
-		generateModel(erpModel)
-		);
-});
 
 var saveOnThumbnail = function (first, second, type) {	
 	$('.src').filter('.src').append('<br/><button class="btn btn-primary ' + type + '" type="button">' + $(first).text() + '</button>');
@@ -107,8 +107,25 @@ var saveIt = function (element, type) {
 		app.merge = element;
 }
 
-$('body').on('click', 'th', function() { 
-	$(this).cellsSelection({
+$('body').on('click', 'th', function(e) { 
+	console.log($(this).css('background-color'));
+	console.log(currentColor);
+	if ($(this).css('background-color') != "rgba(0, 0, 0, 0)" 
+		&& $(this).css('background-color') != 'rgb(255, 255, 255)') {
+		$(this).css('background-color', 'white');
+		$(this).css('color', '#656565');
+	}	
+	else {
+		if (currentColor != null) {
+			$(this).css('background-color', currentColor);
+			$(this).css('color', 'white');
+			$('.boxColored').css('background-color', 'white');
+			currentColor = null;
+		}
+	}
+	
+	
+	/*$(this).cellsSelection({
 		'type'			: 'merge',
 		'onTwoClicked' 	: saveOnThumbnail,
 		'globalSave' 	: app.merge,
@@ -121,6 +138,6 @@ $('body').on('click', 'th', function() {
 		'onTwoClicked' 	: saveOnThumbnail,
 		'globalSave' 	: app.select,
 		'checked'		: $("#selection").attr('checked')
-	}, saveIt);
+	}, saveIt);*/
  });
 
