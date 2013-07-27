7b76668a5ccdff57abb51f713c22049b76bcbfe1
Marc Soufflet <marc.soufflet@softdev.fr>
2013-07-27 21:34:44 +0200

    Création de pages du Bundle Public.
    Pages:
    -Documentation
    -Portail
    -Visualisation des données

--- src/Datacity/PublicBundle/Controller/DefaultController.php
+++ src/Datacity/PublicBundle/Controller/DefaultController.php
@@ -6,9 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function homeAction()
-    {
-    	$response = $this->render('DatacityPublicBundle::home.html.twig');
-        return $response;
-    }
+	public function homeAction()
+	{
+		$response = $this->render('DatacityPublicBundle::home.html.twig');
+		return $response;
+	}
+	public function portalAction()
+	{
+		$response = $this->render('DatacityPublicBundle::portal.html.twig');
+		return $response;
+	}
+	public function documentationAction()
+	{
+		$response = $this->render('DatacityPublicBundle::documentation.html.twig');
+		return $response;
+	}
+	public function dataviewAction()
+	{
+		$response = $this->render('DatacityPublicBundle::dataview.html.twig');
+		return $response;
+	}
 }
