4bc70f46079febb07d8f1f0323fba47b5c21c13c
Marc Soufflet <marc.soufflet@softdev.fr>
2013-07-28 15:01:51 +0200

    Ajout page de detail des applications
    -Ajout de routes
    -Ajout de controller
    -Acces a la page detail: /detail/id (id optionel)

--- src/Datacity/PublicBundle/Controller/DefaultController.php
+++ src/Datacity/PublicBundle/Controller/DefaultController.php
@@ -16,6 +16,19 @@ class DefaultController extends Controller
 		$response = $this->render('DatacityPublicBundle::portal.html.twig');
 		return $response;
 	}
+	public function appDetailAction($id)
+	{
+		$name = "DataCity Tourism";
+		$description = "Application de référencement des principaux lieux culturels, historiques et touristiques.";
+		$cats =  array("Tourisme", "Loisirs", "Histoire");
+		$client = "Montpellier";
+		$platforms =  array("iOs", "Android", "Blackberry", "Windows Phone");
+		$images = array("http://www.caledonianpost.com/wp-content/uploads/2013/04/android.jpg", "http://www.theartoftylerjordan.com/newsite_images/Illustration/concepts/googlebot.jpg");
+		$url = "http://www.google.fr/";
+		$downloaded = 1423;
+		$response = $this->render('DatacityPublicBundle::appDetail.html.twig', array("app_id" => $id, "app_name" => $name, "app_categories" => $cats, "app_client" => $client, "app_platforms" => $platforms, "app_images" => $images, "app_description" => $description, "app_url" => $url, "app_downloaded" => $downloaded));
+		return $response;
+	}
 	public function documentationAction()
 	{
 		$response = $this->render('DatacityPublicBundle::documentation.html.twig');
