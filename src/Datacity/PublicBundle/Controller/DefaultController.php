9fb9750b067fe69763b7503833b8e40082bf5671
Marc Soufflet <marc.soufflet@softdev.fr>
2013-08-04 17:12:34 +0200

    Ajout objets DB Doctrine: Image et Application
    Contenu page portail et detail dynamique.
    Gestion d'objet Application contenant tableau d'objet Image.
    Manque ajout des objets: Platform, Ville, Categorie, Client

--- src/Datacity/PublicBundle/Controller/DefaultController.php
+++ src/Datacity/PublicBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace Datacity\PublicBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Datacity\PublicBundle\Entity\Application;
+use Datacity\PublicBundle\Entity\Image;
 
 class DefaultController extends Controller
 {
@@ -19,11 +21,35 @@ class DefaultController extends Controller
 	public function portalAction()
 	{
 		$villes = array("Montpellier", "Paris", "Lille", "Bordeaux", "Lyon", "Marseille");
-		$projects = array(
-				1 => array('app_id' => 1, 'app_name' => 'Datacity Tourism', 'app_description' => 'Application de référencement des principaux lieux touristiques.', 'app_images' => array("http://www.caledonianpost.com/wp-content/uploads/2013/04/android.jpg", "http://www.theartoftylerjordan.com/newsite_images/Illustration/concepts/googlebot.jpg"), 'app_platforms' => array("iOs", "Android", "Blackberry", "Windows Phone")),
-				2 => array('app_id' => 2, 'app_name' => 'Datacity Culture', 'app_description' => 'Application de référencement des principaux lieux culturels.', 'app_images' => array("http://www.baggy.no/wp-content/uploads/2012/06/google_%C3%A5pner_live_videotjeneste_09052012.png", "http://www.king-david-crown.com/images/temp_flash_window_fr.jpg"), 'app_platforms' => array("iOs", "Android", "Blackberry", "Windows Phone")),
-				3 => array('app_id' => 3, 'app_name' => 'Datacity Street', 'app_description' => 'Application de référencement des principales rues. ^^', 'app_images' => array("http://www.giornalettismo.com/wp-content/uploads/2012/12/screen-shot-2012-12-12-at-11-13-53-pm-770x310.png", "http://www.clandoustphotography.co.uk/images/banner_bridges1.jpg"), 'app_platforms' => array("iOs", "Android", "Blackberry", "Windows Phone")));
-		$response = $this->render('DatacityPublicBundle::portal.html.twig', array('filter_cities' => $villes, 'projects' => $projects));
+		/*$application = new Application();
+		$application->setName('Datacity Street');
+		$application->setDescription('Application de référencement des principales rues. ^^');
+		
+		$image1 = new Image();
+		$image1->setUrl("http://www.giornalettismo.com/wp-content/uploads/2012/12/screen-shot-2012-12-12-at-11-13-53-pm-770x310.png");
+		$image1->setAlt("Premiere image Street");
+		$image1->setApplication($application);
+		
+		$image2 = new Image();
+		$image2->setUrl("http://www.clandoustphotography.co.uk/images/banner_bridges1.jpg");
+		$image2->setAlt("Seconde image Street");
+		$image2->setApplication($application);
+		*/
+				
+		$repo = $this->getDoctrine()->getRepository("DatacityPublicBundle:Application");
+		$applications = $repo->findAll();
+		
+		/*$em = $this->getDoctrine()->getManager();
+		
+		foreach ($applications as $key => $value)
+		{
+			$appli = $applications[$key];
+		}
+		
+		$em->remove($appli);
+		$em->flush();*/
+		
+		$response = $this->render('DatacityPublicBundle::portal.html.twig', array('filter_cities' => $villes, 'apps' => $applications));
 		return $response;
 	}
 	public function appDetailAction($id)
@@ -36,7 +62,24 @@ class DefaultController extends Controller
 		$images = array("http://www.caledonianpost.com/wp-content/uploads/2013/04/android.jpg", "http://www.theartoftylerjordan.com/newsite_images/Illustration/concepts/googlebot.jpg");
 		$url = "http://www.google.fr/";
 		$downloaded = 1423;
-		$response = $this->render('DatacityPublicBundle::appDetail.html.twig', array("app_id" => $id, "app_name" => $name, "app_categories" => $cats, "app_client" => $client, "app_platforms" => $platforms, "app_images" => $images, "app_description" => $description, "app_url" => $url, "app_downloaded" => $downloaded));
+		
+		$apps = $this->getDoctrine()
+		->getRepository('DatacityPublicBundle:Application')
+		->findById($id);
+		
+		foreach ($apps as $value)
+		{
+			$app = $value;
+		}
+		
+		if (!isset($app) || !$app) {
+			throw $this->createNotFoundException(
+					'Aucune application trouvée pour cet id : '.$id
+			);
+		}
+		
+		
+		$response = $this->render('DatacityPublicBundle::appDetail.html.twig', array("appli" => $app, "app_categories" => $cats, "app_client" => $client, "app_platforms" => $platforms, "app_url" => $url, "app_downloaded" => $downloaded));
 		return $response;
 	}
 	public function documentationAction()
