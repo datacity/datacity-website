2fa90056fed1325e75039d8847f7d7a4e37d71f1
messerschmitt755 <ryan.legasal@gmail.com>
2015-01-22 14:49:21 +0100

    pagination producers controller

--- src/Datacity/PublicBundle/Controller/ProducerController.php
+++ src/Datacity/PublicBundle/Controller/ProducerController.php
@@ -5,6 +5,8 @@ namespace Datacity\PublicBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Datacity\PublicBundle\Entity\Producer;
 
+define("PAGINATION_LIMIT", 2);
+
 class ProducerController extends Controller
 {
 	public function listAction()
@@ -12,7 +14,15 @@ class ProducerController extends Controller
 		$em = $this->getDoctrine()->getManager();
 		$producers = $this->getDoctrine()->getRepository("DatacityPublicBundle:Producer")->findAll();
 		
-		$response = $this->render('DatacityPublicBundle::producerList.html.twig', array('producers' => $producers));
+		$dql   = "SELECT a FROM DatacityPublicBundle:Producer a";
+    	$query = $em->createQuery($dql);
+
+    	$paginator  = $this->get('knp_paginator');
+    	$pagination = $paginator->paginate(
+        $query,
+        $this->get('request')->query->get('page', 1),PAGINATION_LIMIT);
+
+		$response = $this->render('DatacityPublicBundle::producerList.html.twig', array('producers' => $producers, 'pagination' => $pagination));
 		return $response;
 	}
 	
