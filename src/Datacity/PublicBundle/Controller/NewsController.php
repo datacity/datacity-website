55d2bd39b86bd2536000012cc0e4765b46de21c7
Guillaume d'Eimar de Jabrun <guillaume.d-eimar-de-jabrun@epitech.eu>
2015-01-26 17:26:22 +0100

    Clean sur les pages news / producteurs

--- src/Datacity/PublicBundle/Controller/NewsController.php
+++ src/Datacity/PublicBundle/Controller/NewsController.php
@@ -5,36 +5,29 @@ namespace Datacity\PublicBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Datacity\PublicBundle\Entity\News;
 
-define("PAGINATION_LIMIT", 2);
-
 class NewsController extends Controller
 {
-
+    const PAGINATION_LIMIT = 2;
 	// Controller de la page News
 	// Dans ce controller nous récupérons également l'ensemble des catégories car nous avons besoins de les réafficher sur la page de news.
 	public function newsAction()
 	{
-		
+
 		$em = $this->getDoctrine()->getManager();
-		
-		
-		// Récupération des news qui sont en base
-		$news = $this->getDoctrine()->getRepository("DatacityPublicBundle:News");
-		$news = $news->findAll();
-		
+
 		$dql   = "SELECT a FROM DatacityPublicBundle:News a";
     	$query = $em->createQuery($dql);
 
     	$paginator  = $this->get('knp_paginator');
     	$pagination = $paginator->paginate(
         $query,
-        $this->get('request')->query->get('page', 1), PAGINATION_LIMIT);
+        $this->get('request')->query->get('page', 1), self::PAGINATION_LIMIT);
 
 		//Redirection vers la page news en envoyant le tableau de news et de categories initialisé précédement.
-		$response = $this->render('DatacityPublicBundle::news.html.twig', array('news' => $news, 'pagination' => $pagination));
+		$response = $this->render('DatacityPublicBundle::news.html.twig', array('pagination' => $pagination));
 		return $response;
 	}
-	
+
 	public function newsDetailAction(News $news)
 	{
 		return $this->render('DatacityPublicBundle::newsDetail.html.twig', array("news" => $news));
--- src/Datacity/PublicBundle/Controller/ProducerController.php
+++ src/Datacity/PublicBundle/Controller/ProducerController.php
@@ -5,29 +5,27 @@ namespace Datacity\PublicBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Datacity\PublicBundle\Entity\Producer;
 
-define("PAGINATION_LIMIT", 2);
-
 class ProducerController extends Controller
 {
+	const PAGINATION_LIMIT = 2;
 	public function listAction()
 	{
 		$em = $this->getDoctrine()->getManager();
-		$producers = $this->getDoctrine()->getRepository("DatacityPublicBundle:Producer")->findAll();
-		
+
 		$dql   = "SELECT a FROM DatacityPublicBundle:Producer a";
     	$query = $em->createQuery($dql);
 
     	$paginator  = $this->get('knp_paginator');
     	$pagination = $paginator->paginate(
         $query,
-        $this->get('request')->query->get('page', 1),PAGINATION_LIMIT);
+        $this->get('request')->query->get('page', 1), self::PAGINATION_LIMIT);
 
-		$response = $this->render('DatacityPublicBundle::producerList.html.twig', array('producers' => $producers, 'pagination' => $pagination));
+		$response = $this->render('DatacityPublicBundle::producerList.html.twig', array('pagination' => $pagination));
 		return $response;
 	}
-	
+
 	public function showAction(Producer $prod)
-	{		
+	{
 		$response = $this->render('DatacityPublicBundle::producerDetail.html.twig', array("producer" => $prod));
 		return $response;
 	}
