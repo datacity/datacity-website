0a4a1443d4192de492da00d8897b521ccae48ae8
Marquis Cyntia <cyntia.marquis@epitech.eu>
2015-01-27 19:16:39 +0100

    add description fixture producteur

--- src/Datacity/PublicBundle/DataFixtures/ORM/ProducerData.php
+++ src/Datacity/PublicBundle/DataFixtures/ORM/ProducerData.php
@@ -32,11 +32,26 @@ class ProducerData extends AbstractFixture implements OrderedFixtureInterface
 			'http://www.datacity.fr/'
 		);
 
+		$description = array (
+			'La plateforme “data.gouv.fr” permet aux services publics de publier des données 
+			 publiques et à la société civile de les enrichir, modifier, interpréter en vue de 
+			 coproduire des informations d’intérêt général.',
+			'A travers ce site, la Ville de Montpellier et ses partenaires mettent à disposition 
+			 des données publiques non nominatives ayant une dimension territoriale.',
+			'Le projet est porté par le Conseil général des Côtes d\'Armor et par ses partenaires : 
+			 Saint-Brieuc Agglomération, Côtes d\'Armor Développement... Désormais, aux collectivités 
+			 et à chacun de se l\'approprier !',
+			'Datacity est un projet qui offre plusieurs fonctionnalités dans le domaine de l\'Open Data. 
+			 Il analyse et centralise les besoins des collectivités territoriales grâce à l\'exploitation
+			 de données libres, et les met à disposition par une interface de programmation (API). 
+			 DataCity offre également un portail applicatif extensible et malléable à la demande des services publics'
+		);
+
 		foreach (self::$prodName as $i => $name)
 		{
 			$prod = new Producer();
 			$prod->setName($name);
-			$prod->setDescription('TODO');
+			$prod->setDescription($description[$i]);
 			$prod->setImage($image[$i]);
 			$prod->setLink($link[$i]);
 			$manager->persist($prod);
